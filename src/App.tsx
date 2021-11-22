0db858cfad15c659027016e140f58da35b8d9418
Junhyun <mnmthatisus@gmail.com>
2021-11-23 08:51:17 +0900

    theory done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper=styled.section`
+  width:100%;
+  height:100%;
+  background-color:${props=>props.theme.backColor};
+  color:${props=>props.theme.textColor};
+`;
+const Body=styled.body`
+`;
 
 function App() {
-  const [text,setText]=useState("");
-  const handleChange=(e: React.FormEvent<HTMLInputElement>)=>{
-    const {value}=e.currentTarget;
-    setText(value);
-  }
-
-  const hadnleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault();
-    console.log(text,"????")
-  }
-  return (
-    <form onSubmit={hadnleSubmit}>
-        <input value={text} onChange={handleChange} type="text" placeholder="User name" />
-        <button type="submit">Login</button>
-    </form>
+  return (<Body>
+    <Wrapper>
+      <h1>hello</h1>
+    </Wrapper>
+  </Body>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme } from './theme';
+import { darkTheme, whiteTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={whiteTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
