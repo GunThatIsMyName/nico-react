26aab3b570b6a528c25f415dbfee0df46b897444
Junhyun <mnmthatisus@gmail.com>
2021-11-23 08:21:32 +0900

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import Circle from "./circle";
+import React, { useState } from "react";
 
 function App() {
+  const [text,setText]=useState("");
+  const handleChange=(e: React.FormEvent<HTMLInputElement>)=>{
+    const {value}=e.currentTarget;
+    setText(value);
+  }
+
+  const hadnleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    console.log(text,"????")
+  }
   return (
-    <div>
-      <Circle text="TEXT" color="tomato" border="black" />
-      <Circle color="teal" />
-      <Circle color="yellow"/>
-    </div>
+    <form onSubmit={hadnleSubmit}>
+        <input value={text} onChange={handleChange} type="text" placeholder="User name" />
+        <button type="submit">Login</button>
+    </form>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { defaultTheme } from "styled-components";
+
+export const darkTheme:defaultTheme={
+    textColor:"white",
+    backColor:"black"
+}
+
+export const whiteTheme:defaultTheme={
+    textColor:"black",
+    backColor:"white"
+}
